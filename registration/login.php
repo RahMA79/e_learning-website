cdd277f1d7d4827b33cfd11bcf13b90f2743c502
RahMA79 <rahoma772004@gmail.com>
2024-12-19 23:51:05 +0200

    make the registration responsive ..

--- registration/login.php
+++ registration/login.php
@@ -39,21 +39,16 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 </head>
 
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+<header>   
+<nav class="navbar navbar-expand-lg bg-primary shadow">
       <div class="container">
-        <!-- Logo -->
         <a class="navbar-brand d-flex align-items-center logo" href="#">
           <img src="../home/images/logo1.png" alt="Logo" width="50" height="50" class="me-2">
           <span>E-Learning</span>
         </a>
-
-        <!-- Toggler for mobile view -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <!-- Navbar links -->
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto me-4">
             <li class="nav-item"><a class="nav-link text-light" href="../home/home.php">Home</a></li>
@@ -62,62 +57,53 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             <li class="nav-item"><a class="nav-link text-light" href="#">My Learning</a></li>
             <li class="nav-item"><a class="nav-link text-light" href="#">About Us</a></li>
           </ul>
-
-          <!-- Search bar -->
           <form class="d-flex me-4">
             <input class="form-control me-2" type="search" placeholder="Search courses..." aria-label="Search">
             <button class="btn btn-outline-light" type="submit">Search</button>
           </form>
-
-          <!-- Cart -->
           <a href="../cart/cart.html" class="me-3 cart">
             <img src="../home/images/cart_icon.png" alt="Cart" width="30" height="30">
           </a>
-
-          <!-- Login and Signup buttons -->
-          <a href="#" class="btn btn-light me-4">Login</a>
-          <a href="signup.php" class="btn btn-warning">Sign Up</a>
+          <a href="login.php" class="btn btn-light me-4">Login</a>
+          <a href="#" class="btn btn-warning">Sign Up</a>
         </div>
       </div>
     </nav>
-  </header>
-  <div class="register" style="display: flex;">
-    <div>
-      <img src="images/login.jpg" alt="" style="margin-right: 80px;">
+</header>
+  <div class="container register">
+    <div class="row">
+      <div class="col-md-7">
+        <img src="images/login.jpg" alt="" class="img-fluid">
+      </div>
+      <div class="col-md-4">
+        <form action="" method="post">
+          <h1>Log in your account</h1>
+          <div>
+            <input id="email" type="email" name="email" placeholder="Email" required>
+          </div>
+          <div>
+            <input id="password" type="password" name="password" placeholder="Password" required>
+          </div>
+          <button type="submit">Log in</button>
+          <br>
+          <h6 style="padding-top:10px;text-align: center;">Forget password?
+            <button type="button" class="btn btn-link p-0 reset-pass" style="text-decoration: underline; font-size: inherit; margin-left: 0;" data-toggle="modal" data-target="#forgetModal">
+              Reset Password
+            </button>
+          </h6>
+          <div class="divider">or login with</div>
+          <h6 style="text-align: center;">
+            Don't have an account? <a href="signup.php">Sign up</a> </h6>
+        </form>
+      </div>
     </div>
-    <form action="" method="post" style="margin-right: 80px;">
-      <h1>
-        Log in your account
-      </h1>
-      <div>
-        <input id="email" type="email" name="email" placeholder="Email" required>
-      </div>
-      <div>
-        <input id="password" type="password" name="password" placeholder="Password" required>
-      </div>
-      <button type="submit">Log in</button>
-      <br>
-      
-      <h6 style="padding-top:10px;text-align: center;">Forget password?
-        <button type="button" class="btn btn-link p-0 reset-pass" style="text-decoration: underline; font-size: inherit; margin-left: 0;" data-toggle="modal" data-target="#forgetModal">
-          Reset Password
-        </button>
-      </h6>
-
-      <div class="divider">or login with</div>
-
-      <h6 style="text-align: center;">
-        Don't have an account? <a href="signup.php">Sign up</a> </h6>
-    </form>
   </div>
-   <!-- forget Modal -->
-   <div class="modal fade" id="forgetModal" tabindex="-1" role="dialog" aria-labelledby="forgetModalLabel" aria-hidden="true">
+  <div class="modal fade" id="forgetModal" tabindex="-1" role="dialog" aria-labelledby="forgetModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header" style="justify-content: space-between;">
           <h5 class="modal-title" id="forgetModalLabel">Reset Password</h5>
-          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"
-            style="background: none; border: none; padding: 0; margin: 0; font-size: 1.5rem; line-height: 1;">
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close" style="background: none; border: none; padding: 0; margin: 0; font-size: 1.5rem; line-height: 1;">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
@@ -136,13 +122,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
           </div>
         </div>
         <div class="modal-footer">
-          <button type="submit" class="btn btn-primary"
-            style="background-color: #C3413B; color: #fff; border-radius: 5px; border: none; 
-         padding: 10px 20px; font-size: 1rem; font-weight: 600; cursor: pointer; 
-         transition: background-color 0.3s ease;"
-            onmouseover="this.style.backgroundColor='#A82E29';"
-            onmouseout="this.style.backgroundColor='#C3413B';">
-            Reset Password</button>
+          <button type="submit" class="btn btn-primary" style="background-color: #C3413B; color: #fff; border-radius: 5px; border: none; padding: 10px 20px; font-size: 1rem; font-weight: 600; cursor: pointer; transition: background-color 0.3s ease;" onmouseover="this.style.backgroundColor='#A82E29';" onmouseout="this.style.backgroundColor='#C3413B';">
+            Reset Password
+          </button>
         </div>
       </div>
     </div>
--- registration/signup.php
+++ registration/signup.php
@@ -27,29 +27,23 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Sign Up</title>
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../home/styles.css">
   <link rel="stylesheet" href="style.css">
-
+  <link rel="stylesheet" href="../home/styles.css">
 </head>
 
-<body style="margin: 10px;">
+<body>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+    <nav class="navbar navbar-expand-lg bg-primary shadow">
       <div class="container">
-        <!-- Logo -->
         <a class="navbar-brand d-flex align-items-center logo" href="#">
           <img src="../home/images/logo1.png" alt="Logo" width="50" height="50" class="me-2">
           <span>E-Learning</span>
         </a>
-
-        <!-- Toggler for mobile view -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <!-- Navbar links -->
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto me-4">
             <li class="nav-item"><a class="nav-link text-light" href="../home/home.php">Home</a></li>
@@ -58,55 +52,48 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             <li class="nav-item"><a class="nav-link text-light" href="#">My Learning</a></li>
             <li class="nav-item"><a class="nav-link text-light" href="#">About Us</a></li>
           </ul>
-
-          <!-- Search bar -->
           <form class="d-flex me-4">
             <input class="form-control me-2" type="search" placeholder="Search courses..." aria-label="Search">
             <button class="btn btn-outline-light" type="submit">Search</button>
           </form>
-
-          <!-- Cart -->
           <a href="../cart/cart.html" class="me-3 cart">
             <img src="../home/images/cart_icon.png" alt="Cart" width="30" height="30">
           </a>
-
-          <!-- Login and Signup buttons -->
           <a href="login.php" class="btn btn-light me-4">Login</a>
           <a href="#" class="btn btn-warning">Sign Up</a>
         </div>
       </div>
     </nav>
   </header>
-  <div class="register" style="display: flex;">
-    <div>
-      <img src="images/login.jpg" alt="" style="margin-right: 80px;">
+  <div class="container register">
+    <div class="row">
+    <div class="col-md-7">
+        <img src="images/login.jpg" alt="" class="img-fluid">
+      </div>
+      <div class="col-md-4">
+        <form action="" method="post">
+          <h1>Sign up for learning</h1>
+          <div>
+            <input id="name" type="text" name="name" placeholder="Full name" required>
+          </div>
+          <div>
+            <input id="email" type="email" name="email" placeholder="Email" required>
+          </div>
+          <div id="signupPassword">
+            <input id="password" type="password" name="password" placeholder="Password" required>
+          </div>
+          <div class="checkbox">
+            <input id="terms" type="checkbox" name="terms" value="terms" required>
+            <label for="terms">I Accept all terms and privacy policy.</label>
+          </div>
+          <button type="submit">Sign up</button>
+          <div class="divider">or signup with</div>
+          <h6 style="text-align: center;">
+            Already have an account? <a href="login.php">Log in</a>
+          </h6>
+        </form>
+      </div>
     </div>
-    <form action="" method="post" style="margin-right: 80px;">
-      <h1>
-        Sign up for learning
-      </h1>
-      <div>
-        <input id="name" type="text" name="name" placeholder="Full name" required>
-      </div>
-
-      <div>
-        <input id="email" type="email" name="email" placeholder="Email" required>
-      </div>
-
-      <div id="signupPassword">
-        <input id="password" type="password" name="password" placeholder="Password" required>
-      </div>
-
-      <div class="checkbox">
-        <input id="terms" type="checkbox" name="terms" value="terms" required>
-        <label for="terms">I Accept all terms and privacy policy.</label>
-      </div>
-      <button type="submit">Sign up</button>
-      <div class="divider">or signup with</div>
-
-      <h6 style="text-align: center;">
-        Already have an account? <a href="login.php">Log in</a> </h6>
-    </form>
   </div>
   <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
