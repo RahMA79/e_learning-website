f5619571dd7e08df54aba9c20c24447c4f45d645
RahMA79 <rahoma772004@gmail.com>
2024-12-24 11:33:56 +0200

    Udated Database

--- e_learning.sql
+++ e_learning.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 23, 2024 at 06:13 PM
--- Server version: 8.0.35
--- PHP Version: 8.2.13
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2024 at 10:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,40 +27,38 @@ SET time_zone = "+00:00";
 -- Table structure for table `course`
 --
 
-DROP TABLE IF EXISTS `course`;
-CREATE TABLE IF NOT EXISTS `course` (
-  `c_name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Course Name',
-  `c_id` int NOT NULL AUTO_INCREMENT COMMENT 'Course ID, Primary key that auto-increments',
-  `c_desc` text COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Description for course',
-  `c_price` varchar(10) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Price of a course',
-  `c_no_of_lessons` int NOT NULL COMMENT 'Number of lessons in a course',
-  `c_image` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Path to course image',
-  `i_id` int NOT NULL DEFAULT '1' COMMENT 'Instructor ID, foreign key from instructor table',
-  `c_level` varchar(15) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Beginner',
-  PRIMARY KEY (`c_id`),
-  KEY `fk_i_id` (`i_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `course` (
+  `c_name` varchar(100) NOT NULL COMMENT 'Course Name',
+  `c_id` int(11) NOT NULL COMMENT 'Course ID, Primary key that auto-increments',
+  `c_desc` text NOT NULL COMMENT 'Description for course',
+  `c_price` varchar(10) NOT NULL COMMENT 'Price of a course',
+  `c_no_of_lessons` int(11) NOT NULL COMMENT 'Number of lessons in a course',
+  `c_image` varchar(100) NOT NULL COMMENT 'Path to course image',
+  `i_id` int(11) NOT NULL DEFAULT 1 COMMENT 'Instructor ID, foreign key from instructor table',
+  `c_level` varchar(15) NOT NULL DEFAULT 'Beginner'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `course`
 --
 
 INSERT INTO `course` (`c_name`, `c_id`, `c_desc`, `c_price`, `c_no_of_lessons`, `c_image`, `i_id`, `c_level`) VALUES
-('Introduction to Python', 101, 'Learn the basics of Python programming, including syntax, variables, and loops.', '49.99', 10, 'images/course-3.jpg', 1, 'Beginner'),
-('Web Development with HTML & CSS', 102, 'Build beautiful and responsive websites using HTML and CSS.', '59.99', 18, 'images/course-3.jpg', 2, 'beginner'),
-('JavaScript Essentials', 103, 'Master the fundamentals of JavaScript to create dynamic web pages.', '69.99', 20, 'images/course-3.jpg', 1, 'Beginner'),
-('Machine Learning Basics', 104, 'An introductory course on machine learning concepts and algorithms.', '89.99', 12, 'images/course-3.jpg', 3, 'Beginner'),
-('Data Science with Python', 105, 'Learn data analysis and visualization techniques using Python.', '99.99', 18, 'images/course-3.jpg', 1, 'Beginner'),
-('Cybersecurity Fundamentals', 106, 'Understand the principles of cybersecurity and learn to protect systems.', '79.99', 10, 'images/course-3.jpg', 4, 'Beginner'),
-('Cloud Computing with AWS', 107, 'Explore cloud computing services and solutions using AWS.', '119.99', 14, 'images/course-3.jpg', 3, 'Beginner'),
-('Mobile App Development with Flutter', 108, 'Create cross-platform mobile apps using Flutter.', '85.99', 18, 'images/hero2.jpg', 2, 'beginner'),
-('SQL for Data Management', 109, 'Learn how to manage and query databases using SQL.', '39.99', 8, 'images/course-3.jpg', 3, 'Beginner'),
-('Artificial Intelligence Foundations', 110, 'Delve into the world of AI and its real-world applications.', '109.99', 16, 'images/course-3.jpg', 4, 'Beginner'),
+('Introduction to Python', 101, 'Learn the basics of Python programming, including syntax, variables, and loops.', '49.00', 10, 'images/python.png', 1, 'Beginner'),
+('Web Development with HTML & CSS', 102, 'Build beautiful and responsive websites using HTML and CSS.', '59.99', 18, 'images/web.jpg', 2, 'beginner'),
+('JavaScript Essentials', 103, 'Master the fundamentals of JavaScript to create dynamic web pages.', '69.99', 20, 'images/javaScript.png', 1, 'Beginner'),
+('Machine Learning Basics', 104, 'An introductory course on machine learning concepts and algorithms.', '89.99', 12, 'images/machine.jpeg', 3, 'Beginner'),
+('Data Science with Python', 105, 'Learn data analysis and visualization techniques using Python.', '99.0', 18, 'images/dataScience.png', 1, 'Beginner'),
+('Cybersecurity Fundamentals', 106, 'Understand the principles of cybersecurity and learn to protect systems.', '79.99', 10, 'images/cyber.jpeg', 4, 'Beginner'),
+('Cloud Computing with AWS', 107, 'Explore cloud computing services and solutions using AWS.', '119.99', 14, 'images/Cloud.jpeg', 3, 'Beginner'),
+('Mobile App Development with Flutter', 108, 'Create cross-platform mobile apps using Flutter.', '85.99', 18, 'images/Mobile.jpg', 2, 'beginner'),
+('SQL for Data Management', 109, 'Learn how to manage and query databases using SQL.', '39.99', 8, 'images/sql.jpeg', 3, 'Beginner'),
+('Artificial Intelligence Foundations', 110, 'Delve into the world of AI and its real-world applications.', '109.99', 16, 'images/AI.jpeg', 4, 'Beginner'),
 ('web', 132, 'best course.', '75.99', 9, 'images/logo1.png', 2, 'Intermediate'),
 ('web', 134, 'best course.', '119', 16, 'images/course-3.jpg', 1, 'advanced'),
 ('web', 135, 'best course.', '119', 16, 'images/logo1.png', 1, 'advanced'),
 ('skills', 136, 'best course.', '25.99', 4, 'images/logo1.png', 2, 'beginner'),
-('skills', 137, 'best course.', '29.99', 4, 'images/logo1.png', 2, 'intermediate');
+('skills', 137, 'best course.', '29.99', 4, 'images/logo1.png', 2, 'intermediate'),
+('Data Science', 138, 'best course.', '99.99', 45, 'images/course-3.jpg', 1, 'Beginner');
 
 -- --------------------------------------------------------
 
@@ -68,17 +66,15 @@ INSERT INTO `course` (`c_name`, `c_id`, `c_desc`, `c_price`, `c_no_of_lessons`, 
 -- Table structure for table `instructor`
 --
 
-DROP TABLE IF EXISTS `instructor`;
-CREATE TABLE IF NOT EXISTS `instructor` (
-  `i_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Instructor''s name',
-  `i_id` int NOT NULL AUTO_INCREMENT COMMENT 'Instructor''s ID, Primary key, auto-incremented',
-  `i_phone` varchar(13) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Instructor''s phone number',
-  `i_email` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Instructor''s email address',
-  `i_image` varchar(30) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Instructor''s Image Path',
-  `i_bio` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Instructor''s Bio',
-  `i_job_desc` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`i_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `instructor` (
+  `i_name` varchar(50) NOT NULL COMMENT 'Instructor''s name',
+  `i_id` int(11) NOT NULL COMMENT 'Instructor''s ID, Primary key, auto-incremented',
+  `i_phone` varchar(13) NOT NULL COMMENT 'Instructor''s phone number',
+  `i_email` varchar(40) NOT NULL COMMENT 'Instructor''s email address',
+  `i_image` varchar(30) NOT NULL COMMENT 'Instructor''s Image Path',
+  `i_bio` varchar(40) NOT NULL COMMENT 'Instructor''s Bio',
+  `i_job_desc` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `instructor`
@@ -100,14 +96,10 @@ INSERT INTO `instructor` (`i_name`, `i_id`, `i_phone`, `i_email`, `i_image`, `i_
 -- Table structure for table `old_cart`
 --
 
-DROP TABLE IF EXISTS `old_cart`;
-CREATE TABLE IF NOT EXISTS `old_cart` (
-  `p_id` int DEFAULT NULL,
-  `s_id` int DEFAULT NULL,
-  `c_id` int DEFAULT NULL,
-  KEY `p_id` (`p_id`),
-  KEY `s_id` (`s_id`),
-  KEY `c_id` (`c_id`)
+CREATE TABLE `old_cart` (
+  `p_id` int(11) DEFAULT NULL,
+  `s_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -126,7 +118,10 @@ INSERT INTO `old_cart` (`p_id`, `s_id`, `c_id`) VALUES
 (5, 2, 108),
 (5, 3, 108),
 (5, 4, 108),
-(35, 24, 101);
+(35, 24, 101),
+(36, 27, 101),
+(37, 27, 102),
+(38, 27, 108);
 
 -- --------------------------------------------------------
 
@@ -134,12 +129,10 @@ INSERT INTO `old_cart` (`p_id`, `s_id`, `c_id`) VALUES
 -- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE IF NOT EXISTS `payment` (
-  `p_id` int NOT NULL AUTO_INCREMENT,
-  `p_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`p_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `payment` (
+  `p_id` int(11) NOT NULL,
+  `p_date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `payment`
@@ -174,7 +167,10 @@ INSERT INTO `payment` (`p_id`, `p_date_time`) VALUES
 (32, '2024-12-21 19:50:46'),
 (33, '2024-12-22 11:31:37'),
 (34, '2024-12-23 13:53:22'),
-(35, '2024-12-23 20:12:54');
+(35, '2024-12-23 20:12:54'),
+(36, '2024-12-23 20:29:46'),
+(37, '2024-12-23 22:43:04'),
+(38, '2024-12-24 10:33:16');
 
 -- --------------------------------------------------------
 
@@ -182,15 +178,13 @@ INSERT INTO `payment` (`p_id`, `p_date_time`) VALUES
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE IF NOT EXISTS `student` (
-  `s_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `s_email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `s_password` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `s_id` int NOT NULL AUTO_INCREMENT,
-  `i_id` int UNSIGNED NOT NULL,
-  PRIMARY KEY (`s_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `student` (
+  `s_name` varchar(50) NOT NULL,
+  `s_email` varchar(50) NOT NULL,
+  `s_password` varchar(50) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `i_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student`
@@ -217,7 +211,8 @@ INSERT INTO `student` (`s_name`, `s_email`, `s_password`, `s_id`, `i_id`) VALUES
 ('Test', 'testing@test.com', 'password', 23, 0),
 ('john', 'john@gmail.com', '45', 24, 0),
 ('John Doe', 'john@inst.com', '45', 25, 1),
-('Jane Smith', 'jane@inst.com', '________', 26, 2);
+('Jane Smith', 'jane@inst.com', '________', 26, 2),
+('Rahma Ashraf', 'rahma@gmail.com', '12345', 27, 0);
 
 -- --------------------------------------------------------
 
@@ -225,12 +220,9 @@ INSERT INTO `student` (`s_name`, `s_email`, `s_password`, `s_id`, `i_id`) VALUES
 -- Table structure for table `student_cart`
 --
 
-DROP TABLE IF EXISTS `student_cart`;
-CREATE TABLE IF NOT EXISTS `student_cart` (
-  `s_id` int DEFAULT NULL,
-  `c_id` int DEFAULT NULL,
-  KEY `s_id` (`s_id`),
-  KEY `c_id` (`c_id`)
+CREATE TABLE `student_cart` (
+  `s_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -240,6 +232,78 @@ CREATE TABLE IF NOT EXISTS `student_cart` (
 INSERT INTO `student_cart` (`s_id`, `c_id`) VALUES
 (24, 103),
 (24, 108);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`c_id`),
+  ADD KEY `fk_i_id` (`i_id`);
+
+--
+-- Indexes for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD PRIMARY KEY (`i_id`);
+
+--
+-- Indexes for table `old_cart`
+--
+ALTER TABLE `old_cart`
+  ADD KEY `p_id` (`p_id`),
+  ADD KEY `s_id` (`s_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`s_id`);
+
+--
+-- Indexes for table `student_cart`
+--
+ALTER TABLE `student_cart`
+  ADD KEY `s_id` (`s_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Course ID, Primary key that auto-increments', AUTO_INCREMENT=139;
+
+--
+-- AUTO_INCREMENT for table `instructor`
+--
+ALTER TABLE `instructor`
+  MODIFY `i_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Instructor''s ID, Primary key, auto-incremented', AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Constraints for dumped tables
